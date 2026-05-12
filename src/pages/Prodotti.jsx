047169ff9b504478f17b250b67211630931b9ad6
Marco Fiordi <marco.fiordi97@gmail.com>
2026-05-12 18:01:01 +0200

    aggiunta fetch per la chiamata API e la lista di prodotti mappata

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
+
 function Prodotti() {
+    const [prodotti, setProdotti] = useState([]);
+
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+            .then((response) => response.json())
+            .then((data) => {
+                setProdotti(data);
+            });
+    }, []);
     return (
         <main>
             <div>Prodotti</div>
             <p>lista dei prodotti</p>
+            <ul>
+                {prodotti.map((prodotto) => (
+                    <li key={prodotto.id}>
+                        <h2>{prodotto.title}</h2>
+                        <p>{prodotto.price} €</p>
+                        <img src={prodotto.image} alt={prodotto.title} />
+                    </li>
+                ))}
+            </ul>
         </main>
-        
+
     )
 }
 export default Prodotti
