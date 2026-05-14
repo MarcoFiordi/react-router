1671dd94a8ef4e990b6c34d7a2df42ac9d99a9c8
Marco Fiordi <marco.fiordi97@gmail.com>
2026-05-14 15:38:38 +0200

    aggiornata la navbar con navlink e bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,22 @@
-import { Link } from "react-router";
+import { NavLink} from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Homepage</Link>
-                </li>
-                <li>
-                    <Link to="/contacts">Chi siamo</Link>
-                </li>
-                <li>
-                    <Link to="/prodotti">Prodotti</Link>
-                </li>
-            </ul>
+        <nav className='navbar navbar-expand-lg bg-dark navbar-dark'>
+            <div className="container">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <NavLink className='nav-link' to="/">Homepage</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className='nav-link' to="/contacts">Chi siamo</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink  className='nav-link' to="/prodotti">Prodotti</NavLink>
+                    </li>
+                </ul>
+            </div>
+
         </nav>
     )
 }
